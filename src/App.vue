152fd1649c0d9199599ba0c0f676e3665c1ad086
Aristoteles Alves <arystotelys@gmail.com>
2026-01-31 18:51:13 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1>Hello_World</h1>
+  </div>
+</template>
